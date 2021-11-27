a3fb464e2ac09d0037e78be89af05663770dd15c
Nehorowo <trianglek4nz@gmail.com>
2021-11-27 08:11:39 +0500

    +routes +sort

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import pizzaLogo from '../assets/img/pizza-logo.svg';
 import Button from './Button';
-
-const Alertus = () => {
-  alert('afafaw');
-};
 
 const Header = () => {
   return (
     <div className="header">
       <div className="container">
-        <div className="header__logo">
-          <img width="38" src={pizzaLogo} alt="Pizza logo" />
-          <div>
-            <h1>React Pizza</h1>
-            <p>самая вкусная пицца во вселенной</p>
+        <Link to="/">
+          <div className="header__logo">
+            <img width="38" src={pizzaLogo} alt="Pizza logo" />
+            <div>
+              <h1>React Pizza</h1>
+              <p>самая вкусная пицца во вселенной</p>
+            </div>
           </div>
-        </div>
+        </Link>
+
         <div className="header__cart">
-          <Button onClick={Alertus} className="button--cart" />
+          <Link to="cart">
+            <Button className="button--cart" />
+          </Link>
         </div>
       </div>
     </div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
-import Button from "./Button";
-import Header from "./Header";
-import Categories from "./Categories";
-
-export { Button, Header, Categories };
+export { default as Button } from './Button';
+export { default as Header } from './Header';
+export { default as Categories } from './Categories';
+export { default as SortPopup } from './SortPopup';
