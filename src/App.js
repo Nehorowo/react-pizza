cc10ec0d7a81f701e18e827da306fa33e3179d5f
Nehorowo <trianglek4nz@gmail.com>
2021-11-30 06:21:32 +0500

    +PropTypes, +PizzaBlock

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import axios from 'axios';
 import { Header } from './components';
 import { Home, Cart } from './pages';
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => setPizzas(data.pizzas));
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home pizzas={pizzas} />} />
           <Route path="cart" element={<Cart />} />
         </Routes>
       </div>
--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import './scss/app.scss';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <Router>
+    <App />
+  </Router>,
+  // </React.StrictMode>
   document.getElementById('root'),
 );
